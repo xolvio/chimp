9146a7eb2fc1f449cbb58cf67b3756adb81d48c0
sam@hatoum.net <sam@hatoum.net>
2015-04-29 19:35:37 -0700

    why google? :)

--- features/step_definitions/webdriver-steps.js
+++ features/step_definitions/webdriver-steps.js
@@ -1,10 +1,9 @@
 var myStepDefinitionsWrapper = function () {
 
   this.When(/^I visit "([^"]*)"$/, function (url, callback) {
-    //this.browser.
-    //  url(url).
-    //  call(callback);
-    callback();
+    this.browser.
+      url(url).
+      call(callback);
   });
 
   this.Then(/^I see the title of "([^"]*)"$/, function (title, callback) {
