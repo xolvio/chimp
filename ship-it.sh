ac1dce91eb1d5cb03d350161d2aef26673694791
Sam@Xolv.io <sam@hatoum.net>
2015-05-25 01:36:30 -0700

    not calling end sessions for phantpm

--- ship-it.sh
+++ ship-it.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 set -e
 npm test
+
+echo Adding files to git
 git add .
+echo Committing files to git
 git commit -m "$1"
+
+echo Patching npm version
 npm version patch
+
+echo Adding files to git again
 git add .
+echo Committing files to git again
 git commit -m "$1"
+
+echo Pushing the commits
 git push
+echo Pushing tags
 git push --tags
+
+echo Publishing to NPM
 npm publish
