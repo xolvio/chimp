eda706a66ba42b93d23385a1b38a51ca2cfc4978
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-07-31 03:21:35 +0200

    Make all WebDriver.io commands global

--- features/step_definitions/webdriver-steps.js
+++ features/step_definitions/webdriver-steps.js
@@ -1,11 +1,11 @@
 var myStepDefinitionsWrapper = function () {
 
   this.When(/^I visit "([^"]*)"$/, function (url) {
-    this.client.url(url);
+    client.url(url);
   });
 
   this.Then(/^I see the title of "([^"]*)"$/, function (title) {
-    expect(this.client.getTitle()).to.equal(title)
+    expect(getTitle()).to.equal(title);
   });
 
 };
