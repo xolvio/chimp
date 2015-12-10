afe2244e2b7bbaf8d73007947726504fc6e4b1bb
Sam Hatoum <sam@xolv.io>
2015-12-09 23:02:12 -0800

    Simplifies mocha spec

--- tests/mocha-spec.js
+++ tests/mocha-spec.js
@@ -1,9 +1,7 @@
 describe('Chimp Mocha', function() {
   describe('Page title', function () {
     it('should be set by the Meteor method @watch', function () {
-      server.call('updateTitle', 'My Title');
-      browser.url(process.env.ROOT_URL);
-      expect(browser.getText('h1')).to.equal('My Title');
+      browser.url('http://google.com');
     });
   });
 });
