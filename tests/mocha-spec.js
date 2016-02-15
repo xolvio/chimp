5dbf582d44c2fc85f2fa6ab392be8b37583a0794
Sam Hatoum <sam@xolv.io>
2016-02-15 08:32:04 -0800

    Converts mocha spec to ES6

--- tests/mocha-spec.js
+++ tests/mocha-spec.js
@@ -1,6 +1,6 @@
-describe('Chimp Mocha', function() {
-  describe('Page title', function () {
-    it('should be set by the Meteor method @watch', function () {
+describe('Chimp Mocha', () => {
+  describe('Page title', () => {
+    it('should be set by the Meteor method @watch', () => {
       browser.url('http://google.com');
     });
   });
