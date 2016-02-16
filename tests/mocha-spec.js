f86489dc2d5bc66c153c1144badc978444ae4d81
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2016-02-16 12:41:41 +0100

    ES2015 support for Mocha tests (fixes #279)

--- tests/mocha-spec.js
+++ tests/mocha-spec.js
@@ -4,4 +4,20 @@ describe('Chimp Mocha', function() {
       browser.url('http://google.com');
     });
   });
+
+  describe('ES2015', function () {
+    it('is supported', function () {
+      const {a, b} = {a: 'foo', b: 'bar'};
+      const arrowFunc = (foo) => foo;
+      class Foo {
+        constructor() {}
+        foo() {}
+      }
+      var object = {
+        foo() {}
+      };
+      const templateString = `Foo`;
+      const [c, ,d] = [1,2,3];
+    });
+  });
 });
