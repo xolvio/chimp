91d89f223229f327c086fe1a3218ef03176b431f
Andrew Hite <andrew@realhq.com>
2016-03-19 02:50:28 -0500

    Add a hook for custom mocha-helpers

--- src/lib/mocha/mocha-wrapper.js
+++ src/lib/mocha/mocha-wrapper.js
@@ -23,6 +23,12 @@ var mocha = new Mocha(mochaOptions);
 
 mocha.addFile(path.join(path.resolve(__dirname, path.join('mocha-helper.js'))));
 
+var customMochaHelperPath = path.resolve(process.cwd(), process.env['chimp.path'], 'mocha-helper.js');
+
+if (fs.existsSync(customMochaHelperPath)) {
+  mocha.addFile(customMochaHelperPath);
+}
+
 // Add each .js file to the mocha instance
 var testDir = process.env['chimp.path'];
 glob.sync(path.join(testDir, '**')).filter(function (file) {
