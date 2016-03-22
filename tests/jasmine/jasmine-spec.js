abe5c3092ddc0382e95c42f4456296a5efef04d6
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2016-03-22 21:21:39 +0100

    Jasmine support end to end test

---
+++ tests/jasmine/jasmine-spec.js
@@ -0,0 +1,23 @@
+describe('Chimp Jasmine', () => {
+  describe('Page title', () => {
+    it('should be set by the Meteor method @watch', () => {
+      browser.url('http://google.com');
+    });
+  });
+
+  describe('ES2015', function () {
+    it('is supported', function () {
+      const {a, b} = {a: 'foo', b: 'bar'};
+      const arrowFunc = (foo) => foo;
+      class Foo {
+        constructor() {}
+        foo() {}
+      }
+      var object = {
+        foo() {}
+      };
+      const templateString = `Foo`;
+      const [c, ,d] = [1,2,3];
+    });
+  });
+});
