56cb4892321f9e1a54deabb9db92972b458c6b82
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2016-04-04 11:43:47 -0700

    Set timeoutsImplicitWait to 3 seconds by default (fixes #199)

--- src/lib/cucumberjs/hooks.js
+++ src/lib/cucumberjs/hooks.js
@@ -12,6 +12,7 @@ module.exports = function hooks() {
     log.debug('[chimp][hooks] Starting BeforeFeatures');
     global.chimpHelper.setupBrowserAndDDP();
     global.chimpHelper.createGlobalAliases();
+    browser.timeoutsImplicitWaitSync(3000);
     log.debug('[chimp][hooks] Finished BeforeFeatures');
     // noinspection JSUnresolvedVariable
     if (global.UserDefinedBeforeFeatures) {
