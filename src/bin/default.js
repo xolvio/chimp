1ea5ee481114464f436a265651f51d4c5f9fb5ea
Sam Hatoum <sam@xolv.io>
2016-06-28 21:47:52 -0700

    Extracts selenium-standalone config to chimp config file

--- src/bin/default.js
+++ src/bin/default.js
@@ -58,6 +58,30 @@ module.exports = {
     waitforInterval: 250,
   },
 
+  // - - - - SELENIUM-STANDALONE
+  seleniumStandaloneOptions: {
+    // check for more recent versions of selenium here:
+    // http://selenium-release.storage.googleapis.com/index.html
+    version: '2.50.1',
+    baseURL: 'https://selenium-release.storage.googleapis.com',
+    drivers: {
+      chrome: {
+        // check for more recent versions of chrome driver here:
+        // http://chromedriver.storage.googleapis.com/index.html
+        version: '2.20',
+        arch: process.arch,
+        baseURL: 'https://chromedriver.storage.googleapis.com'
+      },
+      ie: {
+        // check for more recent versions of internet explorer driver here:
+        // http://selenium-release.storage.googleapis.com/index.html
+        version: '2.50.0',
+        arch: 'ia32',
+        baseURL: 'https://selenium-release.storage.googleapis.com'
+      }
+    }
+  },
+
   // - - - - SESSION-MANAGER  - - - -
   noSessionReuse: false,
 
