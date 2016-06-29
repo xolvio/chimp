f901ac9192f1eb80762c58059905e164e2680ffa
Sam Hatoum <sam@xolv.io>
2016-06-28 21:49:11 -0700

    Removes dist folder

--- dist/lib/utils/escape-reg-exp.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = escapeRegExp;
-function escapeRegExp(string) {
-  return string.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
-}
