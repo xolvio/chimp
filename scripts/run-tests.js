e89f25d849b39f87ec3156d419adf44c27f80bea
Sam Hatoum <sam@xolv.io>
2016-07-13 16:29:21 +0100

    Does not submit results to simian when running locally

--- scripts/run-tests.js
+++ scripts/run-tests.js
@@ -23,6 +23,12 @@ if (isCI) {
 run(0, 'Running Chimp Unit tests', unitTestsCommand);
 run(0, 'Running Chimp Mocha specs in Chrome', 'node ./bin/chimp.js --mocha --path=tests/mocha');
 run(0, 'Running Chimp Jasmine specs in Chrome', 'node ./bin/chimp.js --jasmine --path=tests/jasmine');
+
+if (isCI) {
 run(1, 'Running Chimp Cucumber specs in Chrome', 'node ./bin/chimp.js --tags=~@cli --simianRepositoryId=' + env.SIMIAN_REPOSITORY + ' --simianAccessToken=' + env.SIMIAN_ACCESS_TOKEN);
+} else {
+  run(1, 'Running Chimp Cucumber specs in Chrome', 'node ./bin/chimp.js --tags=~@cli');
+}
+
 run(2, 'Running Chimp Cucumber specs in Firefox', 'node ./bin/chimp.js --browser=firefox --tags=~@cli');
 run(3, 'Running Chimp Cucumber specs in Phantom', 'node ./bin/chimp.js --browser=phantomjs --tags=~@cli');
