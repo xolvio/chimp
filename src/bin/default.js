20610b96a6edb7a0945c76895de61ce336cbb67f
Sam Hatoum <sam@xolv.io>
2016-07-15 12:44:26 +0100

    Bumps selenium & chromedriver versions. Fixes #421

--- src/bin/default.js
+++ src/bin/default.js
@@ -62,13 +62,13 @@ module.exports = {
   seleniumStandaloneOptions: {
     // check for more recent versions of selenium here:
     // http://selenium-release.storage.googleapis.com/index.html
-    version: '2.50.1',
+    version: '2.53.1',
     baseURL: 'https://selenium-release.storage.googleapis.com',
     drivers: {
       chrome: {
         // check for more recent versions of chrome driver here:
         // http://chromedriver.storage.googleapis.com/index.html
-        version: '2.20',
+        version: '2.22',
         arch: process.arch,
         baseURL: 'https://chromedriver.storage.googleapis.com'
       },
