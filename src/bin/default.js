8c1588a0b56c8a603a6936fb894708b76157c1db
Sam Hatoum <sam@xolv.io>
2016-12-03 15:46:43 +0000

    Externalizes mocha command line options and config to the chimp config file

--- src/bin/default.js
+++ src/bin/default.js
@@ -97,13 +97,15 @@ module.exports = {
 
   // - - - - MOCHA  - - - -
   mocha: false,
-  // mochaTags and mochaGrep only work when watch is false (disabled)
-  mochaTags: '',
-  mochaGrep: null,
-  // 'path: './tests',
-  mochaTimeout: 60000,
-  mochaReporter: 'spec',
-  mochaSlow: 10000,
+  mochaCommandLineOptions: ['--color'],
+  mochaConfig: {
+    // tags and grep only work when watch mode is false
+    tags: '',
+    grep: null,
+    timeout: 60000,
+    reporter: 'spec',
+    slow: 10000,
+  },
 
   // - - - - JASMINE  - - - -
   jasmine: false,
