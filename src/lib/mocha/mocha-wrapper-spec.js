922e0d01ffedbf717f3583e59bd2eb6c800cb848
Evan Francis <Evan_Francis@intuit.com>
2017-01-08 16:38:09 -0800

    fix unit tests for --path and --files for mocha-wrapper

--- src/lib/mocha/mocha-wrapper-spec.js
+++ src/lib/mocha/mocha-wrapper-spec.js
@@ -1,30 +1,23 @@
 // import { MochaWrapper } from './mocha-wrapper';
-import Mocha from 'mocha';
 import glob from 'glob';
+import path from 'path';
 
-var filesGlobParam = './src/**/*.spec.js';
-var testDir = './tests/';
-var testDirFiles = ['fileA.js', 'fileB.js'];
-
-class FakeMocha {
-  constructor() {
-    this.aa = 'aa';
-  }
-  run () {
-    console.log('--FakeMocha run()');
-  }
-  addFile(file){
-    console.log('--FakeMocha addFile():', file);
-  }
-}
+const testDir = './tests/';
+const testDirFiles = ['fileA.js', 'fileB.js'];
+const filesGlobValue = './lib/**/*.spec.js';
+const filesGlobFiles = ['./lib/fileA.spec.js', './lib/someDir/fileB.spec.js'];
+const emptyTestsDirPath = '';
 
 describe.only('mocha-wrapper', function () {
   beforeEach(function () {
-    this.mocha = td.replace('mocha', td.object(Mocha));
-    this.path = td.replace('path');
-    this.glob = td.replace('glob', td.object(glob));
+    td.replace('./mocha-fiberized-ui', {});
     td.replace('exit', td.function('exit'));
     td.replace('../babel-register', td.object({}));
+    this.mocha = td.replace('mocha');
+    this.path = td.replace('path', td.object(path));
+    this.glob = td.replace('glob', td.object(glob));
+    process.env.mochaConfig = JSON.stringify({ tags: ''});
+    process.argv = [];
   });
   afterEach(function() {
     td.reset();
@@ -32,30 +25,62 @@ describe.only('mocha-wrapper', function () {
   describe('add files', function () {
     it('adds files inside testDir', function () {
       process.env['chimp.path'] = testDir;
-      process.env.mochaConfig = JSON.stringify({ tags: ''});
-      process.argv = [];
-
+      // mock --path tests dir files
       td.when(this.path.join('mocha-helper.js')).thenReturn('mocha-helper.js');
       td.when(this.path.resolve(__dirname, 'mocha-helper.js')).thenReturn('mocha-helper.js');
       td.when(this.path.join(testDir, '**')).thenReturn(testDir);
       td.when(this.glob.sync(testDir)).thenReturn(testDirFiles);
       td.when(this.mocha.run(td.matchers.anything())).thenReturn(null);
-      require('./mocha-wrapper.js')
-      // const mochaWrapper = new require('./mocha-wrapper.js').MochaWrapper;
-      // td.verify(this.mocha.addFile('fileA.js'));
+
+      const MochaWrapper = require('./mocha-wrapper.js').MochaWrapper;
+      const mochaWrapper = new MochaWrapper();
+
+      td.verify(this.mocha.addFile('fileA.js'));
+      td.verify(this.mocha.addFile('fileB.js'));
+      td.verify(this.mocha.addFile('mocha-helper.js'));
+      td.verify(this.mocha.addFile(), {times: 3, ignoreExtraArgs: true}); // verify only called three times
     });
-    // it('adds files inside testDir', function () {
-    //   process.env['chimp.path'] = testDir;
-    //   process.env.mochaConfig = JSON.stringify({ tags: ''});
-    //   process.argv = [];
-    //
-    //   td.when(this.path.join('mocha-helper.js')).thenReturn('mocha-helper.js');
-    //   td.when(this.path.resolve(__dirname, 'mocha-helper.js')).thenReturn('mocha-helper.js');
-    //   td.when(this.path.join(testDir, '**')).thenReturn(testDir);
-    //   td.when(this.glob.sync(testDir)).thenReturn(testDirFiles);
-    //   td.when(this.mocha.run(td.matchers.anything())).thenReturn(null);
-    //   const mochaWrapper = new require('./mocha-wrapper.js').MochaWrapper;
-    //   td.verify(this.mocha.addFile('fileA.js'));
-    // });
+    it('adds files specified by --files options when --path is empty dir', function() {
+      process.env['chimp.path'] = emptyTestsDirPath;
+      process.env['chimp.files'] = filesGlobValue;
+      // mock empty --path tests dir files
+      td.when(this.path.join('mocha-helper.js')).thenReturn('mocha-helper.js');
+      td.when(this.path.resolve(__dirname, 'mocha-helper.js')).thenReturn('mocha-helper.js');
+      td.when(this.path.join(emptyTestsDirPath, '**')).thenReturn(emptyTestsDirPath);
+      td.when(this.glob.sync(emptyTestsDirPath)).thenReturn([]);
+      // mock --files flag files
+      td.when(this.glob.sync(filesGlobValue)).thenReturn(filesGlobFiles);
+
+      const MochaWrapper = require('./mocha-wrapper.js').MochaWrapper;
+      const mochaWrapper = new MochaWrapper();
+
+      td.verify(this.mocha.addFile('./lib/fileA.spec.js'));
+      td.verify(this.mocha.addFile('./lib/someDir/fileB.spec.js'));
+      td.verify(this.mocha.addFile('mocha-helper.js'));
+      td.verify(this.mocha.addFile(), {times: 3, ignoreExtraArgs: true});
+    });
+    it('adds files specified by --files option and files in --path when both contain files', function() {
+      process.env['chimp.path'] = testDir;
+      process.env['chimp.files'] = filesGlobValue;
+      // mock mocha-helper
+      td.when(this.path.join('mocha-helper.js')).thenReturn('mocha-helper.js');
+      td.when(this.path.resolve(__dirname, 'mocha-helper.js')).thenReturn('mocha-helper.js');
+      // mock --files flag files
+      td.when(this.glob.sync(filesGlobValue)).thenReturn(filesGlobFiles);
+      // mock --path test dir files
+      td.when(this.path.join(testDir, '**')).thenReturn(testDir);
+      td.when(this.glob.sync(testDir)).thenReturn(testDirFiles);
+      td.when(this.mocha.run(td.matchers.anything())).thenReturn(null);
+
+      const MochaWrapper = require('./mocha-wrapper.js').MochaWrapper;
+      const mochaWrapper = new MochaWrapper();
+
+      td.verify(this.mocha.addFile('fileA.js'));
+      td.verify(this.mocha.addFile('fileB.js'));
+      td.verify(this.mocha.addFile('./lib/fileA.spec.js'));
+      td.verify(this.mocha.addFile('./lib/someDir/fileB.spec.js'));
+      td.verify(this.mocha.addFile('mocha-helper.js'));
+      td.verify(this.mocha.addFile(), {times: 5, ignoreExtraArgs: true});
+    });
   });
 });
--- src/lib/mocha/mocha-wrapper.js
+++ src/lib/mocha/mocha-wrapper.js
@@ -1,5 +1,4 @@
 require('../babel-register');
-
 var Mocha = require('mocha'),
   fs = require('fs'),
   path = require('path'),
@@ -25,23 +24,18 @@ class MochaWrapper {
 
     var mocha = new Mocha(mochaConfig);
 
-    console.log('---mocha adding file 0: ', path.join(path.resolve(__dirname, path.join('mocha-helper.js'))));
     mocha.addFile(path.join(path.resolve(__dirname, path.join('mocha-helper.js'))));
-    console.log('--process.argv=', process.argv);
     if (process.argv.length > 3) {
       process.argv.splice(3).forEach(function (spec) {
-        console.log('---mocha adding file argv:', spec);
         mocha.addFile(spec);
       });
     } else {
       // Add each .js file in the tests dir to the mocha instance
       var testDir = process.env['chimp.path'];
-      console.log('--glob.sync (first) path.join(testDir, "**"))=', glob.sync(path.join(testDir, '**')));
       glob.sync(path.join(testDir, '**')).filter(function (file) {
         // Only keep the .js files
         return file.substr(-3) === '.js';
       }).forEach(function (file) {
-        console.log('--mocha adding file --path:', file);
         mocha.addFile(file);
       });
 
@@ -49,7 +43,6 @@ class MochaWrapper {
         // Add each file specified by the "files" option to the mocha instance
         glob.sync(process.env['chimp.files']).forEach(function (file) {
           mocha.addFile(file);
-          console.log('--mocha adding file --files:', file);
         });
       }
 
@@ -57,9 +50,9 @@ class MochaWrapper {
 
     try {
       // Run the tests.
-      console.log('--mocha run returns:', mocha.run(function (failures) {
+      mocha.run(function (failures) {
         exit(failures);
-      }));
+      });
     } catch (e) {
       throw (e);
     }
