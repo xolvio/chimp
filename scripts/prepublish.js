dd3469d9aaf9fefa5dfbf22fa2fd206ea43266bb
Sam Hatoum <sam@xolv.io>
2017-08-11 13:43:19 +0100

    Do not kill the dist dir between watch runs

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -4,6 +4,5 @@ const exec = require('./lib/exec');
 const distDir = packageJson.distDir;
 const chimpDir = packageJson.chimpDir;
 
-exec(`rm -rf ${process.cwd()}/${distDir}`);
 exec(`${process.cwd()}/${chimpDir}/node_modules/.bin/babel ${process.cwd()}/${chimpDir} --ignore *spec,node_modules --out-dir ${process.cwd()}/${distDir} -q`);
 exec(`cp ${process.cwd()}/${chimpDir}/package.json ${process.cwd()}/${distDir}`);
