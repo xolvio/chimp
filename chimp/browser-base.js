ae62f3b4db387d2bb054bdb95761af66b18fd570
Łukasz Gandecki <lgandecki@css.edu>
2017-08-16 21:48:45 +0400

    temporary hack to prevent chimp timeouts

--- chimp/browser-base.js
+++ chimp/browser-base.js
@@ -13,6 +13,17 @@ export default function BrowserFactory({port = 9515, host = '127.0.0.1', desired
   }
   global[`__chromedriver${port}`] = chromedriver;
 
+  //XXX temporary hack to start the debug chromedriver right away, so chimp doesn't time out the first time you try
+  // to run it in debug mode.
+  // We need to figure out how to wait for the chromedriver to startup before initializing the browser,
+  // even though we can't use await/async here. Right now chimp is too fast :-)
+  const debugPort = 9516;
+  if (!global[`__chromedriver${debugPort}`]) {
+    args[1] = `--port=${debugPort}`
+    chromedriver.start(args);
+    global[`__chromedriver${debugPort}`] = chromedriver;
+  }
+
   const options = {
     host,
     port,
