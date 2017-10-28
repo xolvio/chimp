945f536f068ab54845b548baf06c100995997a14
Łukasz Gandecki <lgandecki@css.edu>
2017-10-28 20:43:55 +0400

    attempt to install chrome so chromedriver can start it

---
+++ .circleci/install-chrome.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# sudo dpkg -i google-chrome-stable_current_amd64.deb # this gives information to apt-get about missing dependencies
+sudo apt-get update && sudo apt-get -f -y install
+sudo dpkg -i google-chrome-stable_current_amd64.deb
