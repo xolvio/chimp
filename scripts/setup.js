7b4189f13f62e4de8225e8f9cd3ce92d6a372c5f
Łukasz Gandecki <lgandecki@css.edu>
2017-10-28 19:59:40 +0400

    the issue was with the dist version not being able to run tests, reverted to just running them directly in non-transpiled version for now

--- scripts/setup.js
+++ scripts/setup.js
@@ -2,6 +2,7 @@
 const packageJson = require('../package.json');
 const exec = require('./lib/exec');
 exec(`npm run transpile`);
+exec(`cd ${process.cwd()}/${packageJson.chimpDir} && npm i`);
 exec(`cd ${process.cwd()}/${packageJson.distDir} && npm link`);
 exec(`cd ${process.cwd()}/${packageJson.distDir} && npm i`);
 exec(`cd ${process.cwd()}/${packageJson.testProjectDir} && npm i`);
