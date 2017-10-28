112dfc51a8506b9f010231e30ee3acec4fd88b4b
Łukasz Gandecki <lgandecki@css.edu>
2017-10-28 20:15:37 +0400

    use sudo only for npm link, when on circleci

--- scripts/setup.js
+++ scripts/setup.js
@@ -3,7 +3,12 @@ const packageJson = require('../package.json');
 const exec = require('./lib/exec');
 exec(`npm run transpile`);
 exec(`cd ${process.cwd()}/${packageJson.chimpDir} && npm i`);
-exec(`cd ${process.cwd()}/${packageJson.distDir} && npm link`);
+if (process.env.CIRCLECI) {
+  exec(`cd ${process.cwd()}/${packageJson.distDir} && sudo npm link`);
+}
+else {
+  exec(`cd ${process.cwd()}/${packageJson.distDir} && npm link`);
+}
 exec(`cd ${process.cwd()}/${packageJson.distDir} && npm i`);
 exec(`cd ${process.cwd()}/${packageJson.testProjectDir} && npm i`);
 exec(`npm test`);
