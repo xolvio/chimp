b847a81cc78806fec1577663be9ef786195810b6
Łukasz Gandecki <4002543+lgandecki@users.noreply.github.com>
2021-01-31 13:04:38 +0100

    fix: clean up the cli output (#730)

--- src/generate/helpers/ListrHelper.ts
+++ src/generate/helpers/ListrHelper.ts
@@ -1,5 +1,4 @@
 import { DefaultRenderer } from 'listr2/dist/renderer/default.renderer';
-import logUpdate from 'log-update';
 import debugConfigurator from 'debug';
 import { ListrContext, ListrRendererFactory, ListrTaskWrapper } from 'listr2';
 const debug = debugConfigurator('Listr');
@@ -18,30 +17,5 @@ export const newTask = (
 
 // @ts-ignore
 export class ListrRenderer extends DefaultRenderer {
-  spinner = ['🙈 ', '🙉 ', '🙊 '];
-
-  public render(): void {
-    // Do not render if we are already rendering
-    // @ts-ignore
-    if (this.id) {
-      return;
-    }
-
-    const updateRender = (): void => logUpdate(this.createRender());
-
-    /* istanbul ignore if */
-    if (!this.options?.lazy) {
-      // @ts-ignore
-      this.id = setInterval(() => {
-        // @ts-ignore
-        this.spinnerPosition = ++this.spinnerPosition % this.spinner.length;
-        updateRender();
-      }, 300);
-    }
-
-    // @ts-ignore
-    this.renderHook$.subscribe(() => {
-      updateRender();
-    });
-  }
+  spinner = ['🙈 ', '🙈 ', '🙉 ', '🙉 ', '🙊 ', '🙊 '];
 }
