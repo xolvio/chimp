29e793e78b64f7a011e16f1667f4f4e69ea5b441
lgandecki <gozdak@gmail.com>
2023-08-21 11:58:52 +0200

    feat: do not show so many confusing eslint errors in generated code

--- src/generate/templates/schema.ts
+++ src/generate/templates/schema.ts
@@ -1,6 +1,5 @@
 // if someone wants to use this generator without federation that's completely fine and we don't want this file to cause a typescript error.
 
-// eslint-disable-next-line node/no-missing-import
 import { buildSubgraphSchema } from '@apollo/subgraph';
 import { resolvers } from '{{generatedPrefix}}/graphql/resolvers';
 import gql from 'graphql-tag';
